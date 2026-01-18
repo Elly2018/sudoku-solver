f56322cbed2d45c78efb10b8d728425c12f6bc32
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-01-18 15:05:48 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <v-app>
     <v-main>
-      <v-row>
+      <v-row class="pa-6">
         <v-col>
-          <Sudoku class="sudoku"/>
+          <Sudoku class="sudoku" :step="step"/>
         </v-col>
         <v-col>
           <br /> <br />
           STEP: {{ step_text }}
+          <br /> <br />
+          <v-btn color="primary" v-if="step == 0" @click="step = 1">Finish Puzzle</v-btn>
+          <v-btn color="error" v-if="step == 1" @click="step = 0">Reset Puzzle</v-btn>
+          <v-btn color="primary" v-if="step == 1" @click="step = 2">Start</v-btn>
+          <v-btn color="warning" v-if="step == 2" @click="step = 1">Stop</v-btn>
+          <br /> <br />
+          <v-select v-model.number="type" label="Step Type" hide-details :items="[
+            { title: 'Once', value: 0 },
+            { title: 'GO !', value: 1 },
+          ]"></v-select>
+          <br />
+          <v-select v-model.number="depth" label="Depth" hide-details :items="[
+            { title: '0', value: 0 },
+            { title: '1', value: 1 },
+            { title: '2', value: 2 },
+            { title: '3', value: 3 },
+            { title: '4', value: 4 },
+            { title: '5', value: 5 },
+            { title: '10', value: 10 },
+            { title: '50', value: 50 },
+            { title: '100', value: 100 },
+            { title: 'Unlimited', value: -1 },
+          ]"></v-select>
         </v-col>
       </v-row>
     </v-main>
@@ -29,16 +52,21 @@ export default {
     // 0: Enter init
     // 1: Before solver
     // 2: Process
-    step: 0
+    step: 0,
+    depth: 0,
+    // Processing type
+    // 0: One iteraction only
+    // 1: Go as much as you can
+    type: 0.
   }),
 
   computed: {
     step_text(){
       switch(this.step){
         default:
-        case 0: return "輸入題目";
-        case 1: return "準備階段";
-        case 2: return "正在演算";
+        case 0: return "Enter your puzzle";
+        case 1: return "Prepare";
+        case 2: return "Processing...";
       }
     }
   }
--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -6,9 +6,9 @@
         :class="{ 'borde_black_col': x == 3 || x == 6,
           'borde_black_row': y == 3 || y == 6,
           'sudoku_cursor': x == cursor[0] + 1 && y == cursor[1] + 1
-        }">
+        }" @click="select_cursor(x - 1, y - 1)">
         <p class="sudoku_number">
-          {{ get_value(x - 1, y - 1) == 0 ? '' : get_value(x - 1, y - 1) }}
+          {{ get_value(x - 1, y - 1, currentStep == 0) == 0 ? '' : get_value(x - 1, y - 1, currentStep == 0) }}
         </p>
       </v-col>
     </v-row>
@@ -32,7 +32,13 @@ export default {
     clearInterval(this.loop_handler)
   },
 
+  props: {
+    step: Number,
+    type: Number,
+  },
+  emits: ['finish'],
   data: () => ({
+    currentStep: 0,
     // Init value of the grid
     init: [] ,
     // Trying to answer
@@ -49,11 +55,89 @@ export default {
       clearInterval(this.loop_handler)
       this.loop_handler = setInterval(this.loop, this.timer)
     },
-    loop(){
-
+    answer_reset(){
+      for(let i = 0; i < 81; i++){
+        this.answer[i] = this.init[i]
+      }
     },
-    get_value(x, y){
-      return this.answer[y * 9 + x]
+    total_reset(){
+      for(let i = 0; i < 81; i++){
+        this.init[i] = 0
+        this.answer[i] = 0
+      }
+    },
+    get_grid(x, y){
+      return [
+        this.get_value(y * 3, x * 3),
+        this.get_value(y * 3, x * 3 + 1),
+        this.get_value(y * 3, x * 3 + 2),
+
+        this.get_value(y * 3 + 1, x * 3),
+        this.get_value(y * 3 + 1, x * 3 + 1),
+        this.get_value(y * 3 + 1, x * 3 + 2),
+
+        this.get_value(y * 3 + 2, x * 3),
+        this.get_value(y * 3 + 2, x * 3 + 1),
+        this.get_value(y * 3 + 2, x * 3 + 2),
+      ]
+    },
+    row_colver(num){
+      console.log(num)
+      return false
+    },
+    col_solver(num){
+      console.log(num)
+      return false
+    },
+    grid_solver(num){
+      console.log(num)
+      return false
+    },
+    solver_iteration(){
+      let pass = false
+      for(let i = 1; i < 10; i++){
+        pass = this.grid_solver(i)
+        pass = this.row_colver(i)
+        pass = this.col_solver(i)
+      }
+      return pass
+    },
+    loop(){
+      //console.log(this.get_grid(0, 0))
+      if(this.currentStep != this.step){
+        this.currentStep = this.step
+
+        if(this.currentStep == 0){
+          this.answer_reset()
+        }
+      }
+
+      if(this.currentStep != 2) return;
+      if(this.type == 0){
+        this.solver_iteration()
+        this.$emit('finish')
+      }
+      else if (this.type == 1){
+        let pass = this.solver_iteration()
+        if(!pass) this.$emit('finish')
+      }
+    },
+    select_cursor(x, y){
+      this.cursor = [x, y]
+    },
+    set_value(x, y, v, init=false){
+      if(init){
+        this.init[y * 9 + x] = v
+      }else{
+        this.answer[y * 9 + x] = v
+      }
+    },
+    get_value(x, y, init=false){
+      if(init){
+        return this.init[y * 9 + x]
+      }else{
+        return this.answer[y * 9 + x]
+      }
     },
     keydown(event){
       if(event.key == 'ArrowUp' && this.cursor[1] > 0){
@@ -68,6 +152,10 @@ export default {
       else if(event.key == 'ArrowRight' && this.cursor[0] < 8){
         this.cursor[0] += 1
       }
+
+      else if(event.key >= '0'&& event.key <= '9'){
+        this.set_value(this.cursor[0], this.cursor[1], Number(event.key), this.currentStep == 0)
+      }
     }
   }
 }
@@ -75,10 +163,10 @@ export default {
 
 <style scoped>
 .borde_black_col {
-  border-right: 1px solid black !important;
+  border-right: 3px solid black !important;
 }
 .borde_black_row {
-  border-bottom: 1px solid black !important;
+  border-bottom: 3px solid black !important;
 }
 .sudoku_grid {
   border: 1px solid #11111125;
